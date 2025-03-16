81aa0aeedf5ce55dba26244013f3412379b3b544
Niceae <loganbish98@hotmail.com>
2025-03-16 08:06:10 -0400

    FPS timeing set

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ pygame.init()
 from constants import *
 def main():
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        clock = pygame.time.Clock()
+        dt = 0
         running = True
         while running:
+            dt = clock.tick(60) / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
             screen.fill("black")
             pygame.display.flip()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
 if __name__ == "__main__":
     print ("Starting Asteroids!")
     print ("Screen width: 1280")
